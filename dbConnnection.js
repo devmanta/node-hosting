80f9fc01079c986623667b1f5211d9bddc535455
KimHeeJin <heejin@utransfer.com>
2022-11-21 00:43:07 +0900

    db host

--- dbConnnection.js
+++ dbConnnection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "10.0.0.1",
+    host: "nodemanta.cafe24app.com",
     user: "nodemanta",
     password: "Dlwlrgkwk1!",
     database: "nodemanta"
