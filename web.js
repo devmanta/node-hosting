a7f9775f823238b08ea2fa9aaef66e680b00f119
KimHeeJin <heejin@utransfer.com>
2022-11-21 15:22:28 +0900

    moidfy port to 80

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8001;
+// const port = 8001;
+const port = 80;
 // const con = require('./dbConnnection');
 const axios = require('axios');
 const fs = require('fs');
